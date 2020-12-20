dd4515dc6b426b17f65919c3bc3864605d1aa8ec
azhadsyed <azhadsyed17@gmail.com>
2020-12-20 11:30:34 -0500

    cosmetic changes

--- src/modeling/train.py
+++ src/modeling/train.py
@@ -11,16 +11,21 @@ The preprocessing pipeline follows the design principles laid out here:
 https://towardsdatascience.com/custom-transformers-and-ml-data-pipelines-with-python-20ea2a7adb65
 """
 
-import pandas as pd, numpy as np
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.compose import make_column_transformer
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import train_test_split
 from statistics import mean
 
-np.set_printoptions(threshold=np.inf)
+import numpy as np
+import pandas as pd
+from joblib import dump
+from sklearn.compose import make_column_transformer
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import OneHotEncoder
 
+np.set_printoptions(threshold=np.inf)
+pd.set_option("display.max_columns", None)
+
+# 0. Read the data
 data = pd.read_csv("data.csv")
 categorical_features = data.dtypes[data.dtypes == "object"].index.values
 
@@ -31,14 +36,18 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 # 2. preprocess the features for training
 ct = make_column_transformer(
-    (OneHotEncoder(), categorical_features), remainder="passthrough"
+    (OneHotEncoder(handle_unknown="ignore"), categorical_features),
+    remainder="passthrough",
 )
 gbr = GradientBoostingRegressor()
 
 # 3. Fit and score the model
-pipeline = make_pipeline(ct, gbr)
-pipeline.fit(X_train, y_train)
+model = make_pipeline(ct, gbr)
+model.fit(X_train, y_train)
 
-data["predictions"] = pipeline.predict(X)
+data["predictions"] = model.predict(X)
 data["error"] = abs(data["predictions"] - data["price"])
-print(data.sort_values(by="error", ascending=False))
+print(data.sort_values(by="error", ascending=False).head(20))
+
+# 4. Cache the model for downstream use
+dump(gbr, "model.joblib")
