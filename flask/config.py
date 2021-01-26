c6b8b50ac33ddd24fde0437d382ca7a5ccc4d884
azhadsyed <azhadsyed17@gmail.com>
2021-01-26 13:47:56 -0500

    rename "flask" folder to "web"

--- flask/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-SECRET_KEY = os.urandom(32)
-FLASK_ENV = "development"
-DEBUG = True
-TESTING = True
--- web/app.py
+++ web/app.py
@@ -7,23 +7,48 @@ from flask import Flask, render_template, request, Markup
 from joblib import load
 from treeinterpreter import treeinterpreter as ti
 
-from forms import EstimateForm
+from .forms import EstimateForm
+
+# from here until first function definition, this could all live in some sort of config?
 
 with open("options.json", "r") as f:
     options = json.load(f)
-column_order = options["column order"]  # I'm okay with this being a global
+column_order = options["column order"]  # I'm okay with this being a global...
+categorical_features = options["categorical features"]
+# but I need to couple it with the data/model better somehow...
 
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
 
 # what if there are more models in circulation? Not sure this design will scale
 model = load("model.joblib")  # treat this like a black box, assume it works for now
-feature_names = model.steps[0][
-    1
-].get_feature_names()  # parse and clean the feature names for downstream use
+feature_names = model.steps[0][1].get_feature_names()
 
 
-def clean_features(array, order):
+def form_data_to_dataframe(form_data):
+    dict_for_pandas = {k: [v] for k, v in form_data.items()}
+    dataframe = pd.DataFrame.from_dict(dict_for_pandas)
+    return dataframe[column_order]
+
+
+def predict_and_unpack(model, dataframe) -> (float, float, list):
+    """Accepts an ML model and a dataframe with one row. Returns a tuple with
+    one floating precision estimate, one floating precision bias, and one two-
+    dimensional array of feature names and floating precision contributions."""
+    if len(dataframe) != 1:
+        raise ValueError("dataframe cannot have more than one row.")
+
+    estimate, bias, contributions = ti.predict(
+        model.steps[-1][1],  # model itself -  random forest
+        model.steps[0][1].transform(dataframe),  # transformed observation
+    )
+    estimate = estimate[0, 0]
+    bias, contributions = bias[0], contributions[0]
+    contributions = list(filter(lambda x: x[1] != 0, zip(feature_names, contributions)))
+    return estimate, bias, contributions
+
+
+def clean_features(array, order) -> dict:
     """Accepts array of raw feature contributions. Returns a clean dict of
     contributions where variables are summed by category"""
     sums = {i: 0 for i in order}
@@ -39,7 +64,7 @@ def clean_features(array, order):
     return sums
 
 
-def order_features(bias, features):
+def order_features(bias, features) -> list:
     """Accepts a clean dictionary of features. Returns a UI-friendly array of
     tuples (ordered) explaining feature contributions."""
     ordered_features = []
@@ -52,27 +77,12 @@ def order_features(bias, features):
     return ordered_features
 
 
-def predict_and_unpack(model, dataframe):
-    """This is a helper function to do some much needed unpacking of the values
-    returned by treeinterpreter.predict()."""
-    estimate, bias, contributions = ti.predict(
-        model.steps[-1][1],  # model itself -  random forest
-        model.steps[0][1].transform(dataframe),  # transformed observation
-    )
-    estimate = round(estimate[0, 0], 2)
-    bias, contributions = bias[0], contributions[0]
-    return estimate, bias, contributions
-
-
 def clean_and_style(bias, contributions):
     """This takes the results of a ti.predict() call and converts them into a
     user - friendly HTML table, showing the contribution of each feature"""
-    feature_explanations = list(
-        filter(lambda x: x[1] != 0, zip(feature_names, contributions))
-    )
-    cleaned_features = clean_features(feature_explanations, column_order)
+    cleaned_features = clean_features(contributions, column_order)
     ordered_features = order_features(bias, cleaned_features)
-    contribution_tags = []  # is there some kind of map/reduce to do this?
+    contribution_tags = []
     for i in ordered_features:
         feature, contribution = i
         contribution_tags.append(
@@ -85,36 +95,33 @@ def clean_and_style(bias, contributions):
     return Markup("<table>" + "".join(contribution_tags) + "</table>")
 
 
-def process_form(model, data):
+def process_form(model, form_data):
     """Accepts an machine learning model and a set of user-inputted observations
     about an apartment.
 
     Returns a tuple of a) the model's prediction for that apartment's price, and
     b) an HTML table summarizing the treeinterpreter feature contributions for
     that prediction."""
-    dataframe = pd.DataFrame.from_dict({k: [v] for k, v in data.items()})[column_order]
+    dataframe = form_data_to_dataframe(form_data)
     estimate, bias, contributions = predict_and_unpack(model, dataframe)
     explanation = clean_and_style(bias, contributions)
     return estimate, explanation
 
 
-# refactor - with classes - 10 lines, 316 characters
 @app.route("/", methods=["GET", "POST"])
 def home():
     form = EstimateForm(request.form)
     estimate, explanation = None, None
     if request.method == "POST" and form.validate():
-        # submit gets thrown into the JSON mix by WTForms,
-        # so delete it and we can use form.data as is!
-        data = form.data
-        del data["submit"]
-        estimate, explanation = process_form(model, data)
-    breakpoint()
+        del form.data["submit"]  # pylint: disable=no-member
+        estimate, explanation = process_form(
+            model, form.data  # pylint: disable=no-member
+        )
     return render_template(
         "form.html",
         form=form,
-        estimate=estimate,  # s/b None or a Decimal
-        explanation=explanation,  # s/b None or an HTML table element
+        estimate=round(estimate, 2),
+        explanation=explanation,
     )
 
 
