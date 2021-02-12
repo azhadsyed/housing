a4664fd0534efc985f8dbed5a72a730ccba1bec0
azhadsyed <azhadsyed17@gmail.com>
2021-02-11 19:59:07 -0500

    update __init__to read s3 model

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,37 @@
 import json
 import pickle
+from dotenv import load_dotenv
+import os
+import tempfile
 
 from flask import Flask
-import joblib
+import boto3
 import shap
 from geopy.geocoders import Nominatim
+from joblib import load
 
-from ml.train import RandomForestModel
+if os.path.exists(".env"):
+    load_dotenv()
+
+key_id = os.environ["AWS_ACCESS_KEY_ID"]
+secret_key = os.environ["AWS_SECRET_KEY"]
+
+s3 = boto3.resource("s3", aws_access_key_id=key_id, aws_secret_access_key=secret_key)
 
 with open(".data/options.json", "r") as f:
     options = json.load(f)
 
 app = Flask(__name__)
 
+with tempfile.TemporaryFile() as fp:
+    s3.download_fileobj("nycestimator", "model.joblib", fp)
+    app.model = load(fp)
+
 app.model_columns = options["column order"]
 app.categorical_features = options["categorical features"]
-
-rfm = RandomForestModel()
-rfm.train_random_forest(".data/data.csv", "price", ["id"])
-
-app.model = rfm.model
 app.feature_names = app.model.steps[0][1].get_feature_names()
 app.explainer = shap.TreeExplainer(app.model.steps[1][1])
-
 app.nominatim = Nominatim(user_agent="nyc_rent_estimator")
-
 app.ti_enabled = False
 
 from app import views
